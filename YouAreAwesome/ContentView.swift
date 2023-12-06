63ba2c8faec08e8c15f7e7bc1fffd2c25d77a4d2
Vishwas Sharma <ch0nky@protonmail.com>
2023-12-06 12:02:02 +0530

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Vishwas Sharma on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var num = 0
+    var body: some View {
+        VStack {
+          
+            Text("\(num)")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+            HStack {
+                Button("Increment!") {
+                    num += 1
+                }
+                .buttonStyle(.bordered)
+                
+                Button("Decrement"){
+                    num -= 1                }
+            }
+             .buttonStyle(.bordered)
+            
+            
+            
+            
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Vishwas Sharma on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
